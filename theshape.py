fd2050325290761b6c897beaba43ed97327041f2
Pessi Vilppolahti <pessiv@outlook.com>
2021-03-29 19:12:30 +0300

    added some functions

--- theshape.py
+++ theshape.py
@@ -10,6 +10,24 @@ def detectCollision(position,checkList):
     return [False,checkList]
 #TODO: growTheShapeNTimes(n)
 #TODO: countInTheNewPositions()
+#calculateTheDiagonals(n)
+def calculateTheDiagonals(n):
+    x0 = 1
+    x = range(n)
+    for n1 in x:
+        if n1 % 2 == 0:
+            #do nothing
+            u=1
+        else:
+            x0+=1
+    return x0
+def calculateTheHorizontals(n):
+    x0 = 0
+    x = range(n)
+    for n1 in x:
+        if n1 % 2 == 0:
+            x0 += 2
+    return x0
 #TODO: calculateNewPositions()
 def calculateNewPositions(position):
     isSideWays = False
@@ -21,4 +39,12 @@ def calculateNewPositions(position):
 #emptyTheNewPositions()
 def emptyTheNewPositions():
     newpositions.clear()
-print(calculateNewPositions([0,0]))
+def areaOfAnOctagon(n):
+    a1 = (n*calculateTheDiagonals(n))/2
+    a2 = (n*calculateTheHorizontals(n))/2
+    return 4*a1+4*a2
+def printStuff(y):
+    x = range(y)
+    for n in x:
+        print(calculateTheHorizontals(n),calculateTheDiagonals(n),4*calculateTheDiagonals(n),4*calculateTheHorizontals(n))
+print(areaOfAnOctagon(9000000))
