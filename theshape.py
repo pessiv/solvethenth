8cdf0e481a82031bc6246e84110cc596344924af
Pessi Vilppolahti <pessiv@outlook.com>
2021-03-29 12:09:53 +0300

    worked on calculateNewPositions()

--- theshape.py
+++ theshape.py
@@ -1,17 +1,24 @@
 positions=[[0,1,"alive"],[1,0,"alive"],[-1,0,"alive"],[0,-1,"alive"]]
 newpositions=[]
-turn = 1
-
+turns = 1
 #detectCollision(position)
-def detectCollision(position):
-    for i in range(len(positions)):
-        if position[0] == positions[i][0] and position[1] == positions[i][1]:
-            positions[i][2] = "dead"
-            return True
+def detectCollision(position,checkList):
+    for i in range(len(checkList)):
+        if position[0] == checkList[i][0] and position[1] == checkList[i][1]:
+            checkList[i][2] = "dead"
+            return [True,checkList]
     return False
 #TODO: growTheShapeNTimes(n)
 #TODO: countInTheNewPositions()
 #TODO: calculateNewPositions()
+def calculateNewPositions(position):
+    isSideWays = False
+    if turns % 2 == 0:
+        isSideWays = True
+    newposes = [[0,0,"alive"],[0,0,"alive"]]
+    #TODO: actually calculate the new positions
+    return newposes
 #emptyTheNewPositions()
 def emptyTheNewPositions():
     newpositions.clear()
+print(calculateNewPositions([0,0]))
